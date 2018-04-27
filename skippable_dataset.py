ffc6de2474f0e399de0c18b7fcb1bb3e7a9e2b27
KatoTetsuro <katotetsuro@team-lab.com>
2018-04-27 18:49:04 +0900

    読めないデータはignore labelで飛ばすdataset

---
+++ skippable_dataset.py
@@ -0,0 +1,11 @@
+import chainer
+import numpy as np
+
+
+class SkippableDataset(chainer.datasets.LabeledImageDataset):
+    def get_example(self, index):
+        try:
+            return super().get_example(self, index)
+        except:
+            print('failed to load data index={}'.format(index))
+            return np.zeros((3, 224, 224), dtype=np.uint8), -1
