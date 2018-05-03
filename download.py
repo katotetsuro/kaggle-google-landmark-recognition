6f004536526c2a4a1ff619e842e34304b9a107f0
KatoTetsuro <katotetsuro@team-lab.com>
2018-05-03 11:26:49 +0900

    リサイズ&EXIFの削除を追加

--- download.py
+++ download.py
@@ -8,6 +8,8 @@ import multiprocessing as multi
 import argparse
 from tqdm import tqdm
 from pathlib import Path
+from io import BytesIO
+from PIL import Image
 
 
 def download_image(box):
@@ -16,9 +18,14 @@ def download_image(box):
         try:
             r = requests.get(box['url'], stream=True)
             if r.status_code == 200:
-                with open(file_path, "wb") as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
+                img = Image.open(BytesIO(r.content))
+                w, h = img.size
+                s = 224 / min(w, h)
+                img = img.resize((int(w*s), int(h*s)), Image.BILINEAR)
+                data = list(img.getdata())
+                img = Image.new(img.mode, img.size)
+                img.putdata(data)
+                img.save(file_path)
                 return box, True
             else:
                 return box, False
@@ -34,10 +41,14 @@ def main():
     parser.add_argument('--file', default='train.csv',
                         help='kaggleで配布されているtrain.csv or test.csvの場所')
     parser.add_argument('--out', default='data', help='ダウンロード先ディレクトリ')
-    parser.add_argument('--txt', default='all.txt', help='ダウンロードに成功した画像の一覧')
+    parser.add_argument('--txt', default='all.txt',
+                        help='ダウンロードに成功した画像の一覧')
     parser.add_argument('--retry', default=None, type=str,
                         help='これを指定した場合は、このリスト内から再ダウンロードする. check_image.pyと併せて使う')
-    parser.add_argument('--limit', default=-1, type=int, help='最大ダウンロード数')
+    parser.add_argument('--limit', default=-1,
+                        type=int, help='最大ダウンロード数')
+    parser.add_argument('--resolution', default=-1,
+                        type=int, help='ダウンロードする解像度')
     args = parser.parse_args()
     print(args)
 
